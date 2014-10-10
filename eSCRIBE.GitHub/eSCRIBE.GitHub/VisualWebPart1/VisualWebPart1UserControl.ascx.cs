f2ed5a2367c445a949af111dc464e4f51a9c05b7
bensonlu2 <blu@escribecorporate.com>
2014-10-10 08:15:09 -0400

    #1356 - None to Male
    None to Male

--- eSCRIBE.GitHub/eSCRIBE.GitHub/VisualWebPart1/VisualWebPart1UserControl.ascx.cs
+++ eSCRIBE.GitHub/eSCRIBE.GitHub/VisualWebPart1/VisualWebPart1UserControl.ascx.cs
@@ -17,7 +17,7 @@ namespace eSCRIBE.GitHub.VisualWebPart1
             tbResult1.Text = "Saved name and company";
 			tbResult2.Text = "";
 			tbAge.Text = "0";
-			tbGender.Text = "None";
+			tbGender.Text = "Male";
         }
 
         protected void btnSaveTable2_Click(object sender, EventArgs e)
